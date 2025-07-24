e9c0a03987e0a90de1ea353e41b1d7711fff9fba
ShahzaibParvaiz <shahzaibparvaizbeigh@gmail.com>
2025-07-24 12:34:47 +0530

    [BUILD] snake_atif.c : snake body + updation + game over logic , game working complete

--- snake_atif.c
+++ snake_atif.c
@@ -10,13 +10,6 @@ int main(int argc, char *argv[]) { //why main taking args - can take when main i
     SDL_Renderer *renderer;
     SDL_Event event; //study event - event polling - alternate methods instead of polling
 
-    //snake_head is called object of the struct, SDL_Rect that we are using.
-    SDL_Rect snake_head;
-    snake_head.x = 320;
-    snake_head.y = 240;
-    snake_head.w = 40;
-    snake_head.h = 40;
-
     //Giving seed to rand() function by time(NULL) which returns current time
     srand(time(NULL));
     //Our grid is w*h = 640*480 -> Height is 16*(40) and Width is 12*(40) - This is the concept behind enum grid
@@ -25,11 +18,31 @@ int main(int argc, char *argv[]) { //why main taking args - can take when main i
     SDL_Rect food;
     food.x = (rand()%X_FACTORS)*GRID_SIDE;
     food.y = (rand()%Y_FACTORS)*GRID_SIDE;
-    food.w = 40;
-    food.h = 40;
+    food.w = GRID_SIDE;
+    food.h = GRID_SIDE;
+
+    //snake_head is called object of the struct, SDL_Rect that we are using.
+    SDL_Rect snake_head;
+    snake_head.x = 320;
+    snake_head.y = 240;
+    snake_head.w = GRID_SIDE;
+    snake_head.h = GRID_SIDE;
 
     int score = 0;
+
+    //Making body as an array
+    //here is the issue. Score=0 and we try to access elements like 1,2,3 etc in loop
+    //The fix seems to declare a large array like 100 elements or DMA - Baaki zaanaan Rabb
+    //TODO - Dynamic Memory Allocation
+    // int body_elements=0;
+    SDL_Rect *snake_body;
+    snake_body=(SDL_Rect*)malloc(sizeof(SDL_Rect));
+
+
     int running = 1;
+    int body_elements = 1;
+    snake_body[0].x = snake_head.x - GRID_SIDE;
+    snake_body[0].y = snake_head.y;
 
     //For direction manipulation
     typedef enum {UP, DOWN, LEFT, RIGHT} Direction;
@@ -41,11 +54,11 @@ int main(int argc, char *argv[]) { //why main taking args - can take when main i
     }
 
     window = SDL_CreateWindow("SNAKE GAME",
-                              SDL_WINDOWPOS_CENTERED, 
-                              SDL_WINDOWPOS_CENTERED, 
-                              640, 480, 
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              640, 480,
                               SDL_WINDOW_SHOWN);
-    
+
     if (window == NULL) {
         printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
@@ -66,6 +79,11 @@ int main(int argc, char *argv[]) { //why main taking args - can take when main i
     // }
 
     while (running) {
+
+        for (int i=0; i<score; i++) {
+            snake_body[i].w = GRID_SIDE;
+            snake_body[i].h = GRID_SIDE;
+        }
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
         //TODO - why RenderClear after SetRenderDrawColor
         //Bcz it clears the complete back buffer with the color we provide in the latest SetRenderDrawColor
@@ -74,6 +92,12 @@ int main(int argc, char *argv[]) { //why main taking args - can take when main i
         SDL_RenderDrawRect(renderer, &food);
         SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);//here it fills the brush for snake head, nothing with the window
         SDL_RenderFillRect(renderer, &snake_head);//here it paints the snake head on canvas
+
+        for (int elements=0; elements<body_elements; elements++) {
+            SDL_SetRenderDrawColor(renderer, 47, 62, 70, 255);
+            SDL_RenderDrawRect(renderer, &snake_body[elements]);
+            SDL_RenderFillRect(renderer, &snake_body[elements]);
+        }
         SDL_SetRenderDrawColor(renderer, 255, 192, 203, 255);//here it changes the brush to pink color
         SDL_RenderFillRect(renderer, &food);//here it draws the food on the canvas using pink brush
         SDL_RenderPresent(renderer); //This function presents everything (the canvas) onto the screen - if it's not called, nothing draws
@@ -84,18 +108,63 @@ int main(int argc, char *argv[]) { //why main taking args - can take when main i
         //TODO: Motion of Snake_Head - Now incorporating Events (Button i/p from keyboard)
         switch (direction) {
             case UP:
+
+                for (int a=body_elements-1; a>=0; a--){
+                    if (a!=0) {
+                        snake_body[a].y = snake_body[a-1].y;
+                        snake_body[a].x = snake_body[a-1].x;
+                    }
+                    else {
+                        snake_body[a].y = snake_head.y;
+                        snake_body[a].x = snake_head.x;
+                    }
+                }
                 snake_head.y = snake_head.y - GRID_SIDE;
                 break;
 
             case DOWN:
+
+                for (int a=body_elements-1; a>=0; a--){
+                    if (a!=0) {
+                        snake_body[a].y = snake_body[a-1].y;
+                        snake_body[a].x = snake_body[a-1].x;
+                    }
+                    else {
+                        snake_body[a].y = snake_head.y;
+                        snake_body[a].x = snake_head.x;
+                    }
+                }
                 snake_head.y = snake_head.y + GRID_SIDE;
                 break;
 
             case LEFT:
+
+                for (int a=body_elements-1; a>=0; a--){
+
+                    if (a!=0) {
+                        snake_body[a].y = snake_body[a-1].y;
+                        snake_body[a].x = snake_body[a-1].x;
+                    }
+                    else {
+                        snake_body[a].y = snake_head.y;
+                        snake_body[a].x = snake_head.x;
+                    }
+                }
                 snake_head.x = snake_head.x - GRID_SIDE;
                 break;
 
             case RIGHT:
+
+                for (int a=body_elements-1; a>=0; a--){
+                    if (a!=0) {
+                        snake_body[a].y = snake_body[a-1].y;
+                        snake_body[a].x = snake_body[a-1].x;
+                    }
+                    else {
+                        snake_body[a].y = snake_head.y;
+                        snake_body[a].x = snake_head.x;
+                    }
+                }
                 snake_head.x = snake_head.x + GRID_SIDE;
                 break;
         }
@@ -115,28 +184,72 @@ int main(int argc, char *argv[]) { //why main taking args - can take when main i
                                 if (direction != DOWN) {
                                     direction = UP;
                                     printf("Arrow Up key is pressed.\n");
+
+                                    for (int a=body_elements-1; a>=0; a--){
+                                        if (a!=0) {
+                                            snake_body[a].y = snake_body[a-1].y;
+                                            snake_body[a].x = snake_body[a-1].x;
+                                        }
+                                        else {
+                                            snake_body[a].y = snake_head.y;
+                                            snake_body[a].x = snake_head.x;
+                                        }
+                                    }
                                     snake_head.y -= GRID_SIDE; //why minus? inverted coordinate system on screen
                                 }break;
 
                             case SDLK_DOWN:
                                 if (direction != UP) {
                                     direction = DOWN;
-                                    snake_head.y += GRID_SIDE;
+
+                                    for (int a=body_elements-1; a>=0; a--){
+                                        if (a!=0) {
+                                            snake_body[a].y = snake_body[a-1].y;
+                                            snake_body[a].x = snake_body[a-1].x;
+                                        }
+                                        else {
+                                            snake_body[a].y = snake_head.y;
+                                            snake_body[a].x = snake_head.x;
+                                        }
+                                    }
                                     printf("Arrow Down key is pressed.\n");
+                                    snake_head.y += GRID_SIDE;
                                 }break;
 
                             case SDLK_LEFT:
                                 if (direction != RIGHT){
                                     direction = LEFT;
-                                    snake_head.x -= GRID_SIDE;
+
+                                    for (int a=body_elements-1; a>=0; a--){
+                                        if (a!=0) {
+                                            snake_body[a].y = snake_body[a-1].y;
+                                            snake_body[a].x = snake_body[a-1].x;
+                                        }
+                                        else {
+                                            snake_body[a].y = snake_head.y;
+                                            snake_body[a].x = snake_head.x;
+                                        }
+                                    }
                                     printf("Arrow Left key is pressed.\n");
+                                    snake_head.x -= GRID_SIDE;
                                 }break;
 
                             case SDLK_RIGHT:
                                 if (direction != LEFT) {
                                     direction = RIGHT;
-                                    snake_head.x += GRID_SIDE;
+
+                                    for (int a=body_elements-1; a>=0; a--){
+                                        if (a!=0) {
+                                            snake_body[a].y = snake_body[a-1].y;
+                                            snake_body[a].x = snake_body[a-1].x;
+                                        }
+                                        else {
+                                            snake_body[a].y = snake_head.y;
+                                            snake_body[a].x = snake_head.x;
+                                        }
+                                    }
                                     printf("Arrow Right key is pressed.\n");
+                                    snake_head.x += GRID_SIDE;
                                 }break;
                         }
             }
@@ -145,13 +258,23 @@ int main(int argc, char *argv[]) { //why main taking args - can take when main i
         boundary_check(&snake_head.x, &snake_head.y);
         //Changing the Position of food if Snake Head arrives at the same coordinates - Eats food, New Food
         if (snake_head.x == food.x && snake_head.y == food.y) {
-            // srand(time(NULL));
             food.x = (rand()%X_FACTORS)*GRID_SIDE;
             food.y = (rand()%Y_FACTORS)*GRID_SIDE;
-            score+=10;
+            score+=1;
+            body_elements+=1;
+            snake_body=(SDL_Rect*)realloc(snake_body,body_elements*sizeof(SDL_Rect));
         }//this if statement end
+        //Checking for collision with body
+        for (int a=0; a<body_elements;a++) {
+            if ((snake_head.x==snake_body[a].x)&&(snake_head.y==snake_body[a].y)) {
+                running = 0;
+                printf("Game Over!\n");
+                break;
+            }
+        }
     }//first while end
     printf("Score: %d\n", score);
+    free(snake_body);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
